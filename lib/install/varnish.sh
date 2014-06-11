c20803daea32f5b1b0890f25113cbb6f17d9ab64
Cameron Drake <cameron@drake.fm>
2014-06-10 18:51:50 -0700

    updates

--- lib/install/varnish.sh
+++
@@ -1,7 +0,0 @@
-#-------------------
-# Install Varnish via https://www.varnish-cache.org/installation/ubuntu
-#-------------------
-curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
-echo "deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y varnish
