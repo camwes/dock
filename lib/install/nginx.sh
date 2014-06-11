4e0d6419595ee96f18d805a945652b6fa43b4e88
Cameron Drake <cameron@drake.fm>
2014-06-10 18:58:03 -0700

    updates

--- lib/install/nginx.sh
+++ lib/install/nginx.sh
@@ -3,18 +3,27 @@
 # Install Nginx
 #------------------
 # Installation from source:
-# wget http://nginx.org/download/nginx-1.4.3.tar.gz
+wget http://nginx.org/download/nginx-1.4.3.tar.gz
 # --------------------------
 # tar xvzf (.tgz or tar.gz)
 # tar xvjf (.tbz or .tar.bz2)
 # tar xvf (.tar)
 # --------------------------
-# sudo tar xvf nginx-1.4.3.tar.gz
-# cd nginx-1.4.3
-# sudo ./configure
-# sudo make
-# sudo make install
-# sudo ln -s /sbin/nginx /usr/sbin
-sudo add-apt-repository ppa:nginx/stable  # use nginx=development for latest development version
+sudo tar xvf nginx-1.4.3.tar.gz
+cd nginx-1.4.3
+sudo ./configure --prefix=/opt/nginx
+sudo make
+sudo make install
+sudo ln -s /sbin/nginx /usr/sbin
+cd -
+# use nginx=development for latest development version
+# sudo apt-add-repository ppa:nginx/stable
+# sudo apt-get update
+# sudo apt-get install -y nginx
+#-------------------
+# Install Varnish via https://www.varnish-cache.org/installation/ubuntu
+#-------------------
+curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
+echo "deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0" | sudo tee -a /etc/apt/sources.list 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y --force-yes varnish
--- lib/install/node.sh
+++ lib/install/node.sh
@@ -3,12 +3,11 @@
 #------------------
 # Install Node Stable
 #------------------
-sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
 # https://npmjs.org/ cache clean just in case
 sudo npm cache clean
-# install global modules
+# install global modules Grunt, Forever, Bower, Coffee, Node-Inspector
 sudo npm install -g bower grunt-cli forever coffee-script node-inspector
